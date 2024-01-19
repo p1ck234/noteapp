3814f548d3947068a0e6e4455cf79cbbc359b9d0
p1ck23 <danil-vetrov47@mail.ru>
2024-01-19 16:08:32 +0400

    did the authorization.

---
+++ JS/app.js
@@ -0,0 +1,40 @@
+let accounts = [
+  {
+    login: "p1ck23",
+    password: "123",
+  },
+];
+
+const authAccount = () => {
+  const inputElementLogin = document.getElementById("inputLogin");
+  const inputElementPassword = document.getElementById("inputPassword");
+
+  for (account of accounts) {
+    if (
+      account.login === inputElementLogin.value &&
+      account.password == inputElementPassword.value
+    ) {
+      window.location.href = "notes.html";
+    } else {
+      alert("Данные не верны!");
+    }
+  }
+};
+
+function formatDate(date) {
+  const day = ("0" + date.getDate()).slice(-2);
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const year = date.getFullYear().toString().slice(-2);
+  const hours = ("0" + date.getHours()).slice(-2);
+  const minutes = ("0" + date.getMinutes()).slice(-2);
+
+  const formattedDate =
+    day + "." + month + "." + year + " " + hours + ":" + minutes;
+
+  return formattedDate;
+}
+
+const headerDateElement = document.getElementsByClassName("header_date");
+const date = new Date();
+console.log(headerDateElement.value);
+headerDateElement[0].textContent = formatDate(date);
